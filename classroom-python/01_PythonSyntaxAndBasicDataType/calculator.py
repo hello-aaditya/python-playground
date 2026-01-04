bfeb43b62ee20d3d7ad6cb051c584eaf60b84639
hello-aaditya <aadityakumar.prof@gmail.com>
2026-01-04 12:25:09 +0530

    Basic Calculator

---
+++ classroom-python/01_PythonSyntaxAndBasicDataType/calculator.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+remainder = a % b
+
+print(a, "+", b,":", addition)
+print(a, "-", b,":", subtraction)
+print(a, "*", b,":", multiplication)
+print(a, "/", b,":", division)
+print(a, "%", b,":", remainder)
