cf5eb77d6da45ef36af4edd3171232dc4fb7d289
hello-aaditya <aadityakumar.prof@gmail.com>
2026-01-04 12:24:55 +0530

    Different DataTypes

---
+++ classroom-python/01_PythonSyntaxAndBasicDataType/dataType.py
@@ -0,0 +1,55 @@
+# Integers -> int
+# Very Short Number
+print("*************************************************\n")
+number1 = 10
+print("Number1 = ", number1)
+print("DataType of '",number1, "' is: ", type(number1))
+
+print("*************************************************\n")
+
+# Very Big Number
+number2 = 101234567890123456
+print("Number2 = ", number2)
+print("DataType of '",number2, "' is:", type(number2))
+
+print("*************************************************\n")
+
+# Decimal Numbers -> Float
+# Very Short decimal numbers
+number3 = 10.55
+print("Number3 = ", number3)
+print("DataType of '",number3, "' is: ", type(number3))
+
+print("*************************************************\n")
+
+# Very Big decimal Number
+number4 = 10.55123456789132456
+print("Number4 = ", number4)
+print("DataType of '", number4, "' is: ", type(number4))
+
+print("*************************************************\n")
+
+# Boolean
+isCorrect = True
+print("isCorrect: ", isCorrect)
+print("DataTYpe of '", isCorrect, "' is: ", type(isCorrect))
+
+print("*************************************************\n")
+
+isCorrect = False
+print("DataType of '", isCorrect, "' is: ", type(isCorrect))
+
+print("*************************************************\n")
+
+
+# String
+word = "Apple"
+print("Word: ", word)
+print("DataType of '", word, "'is: ", type(word))
+
+print("*************************************************\n")
+
+# Complex Number
+complexNumber = 1 + 2j
+print("Complex Number: ", complexNumber)
+print("DataType of Complex Number- '", complexNumber, "' is: ", type(complexNumber))
