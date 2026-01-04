530dbcf61f115ff2fcd14bc729a500880729f223
hello-aaditya <aadityakumar.prof@gmail.com>
2026-01-04 12:24:37 +0530

    Take User Input

---
+++ classroom-python/01_PythonSyntaxAndBasicDataType/takeUserInputPerformCalculation.py
@@ -0,0 +1,14 @@
+number1 = int(input("ENTER FIRST NUMBER: "))
+number2 = int(input("ENTER SECOND NUMBER: "))
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+remainder = number1 % number2
+
+print(number1, "+", number2,"=", addition)
+print(number1, "-", number2,"=", subtraction)
+print(number1, "*", number2,"=", multiplication)
+print(number1, "/", number2,"=", division)
+print(number1, "%", number2,"=", remainder)
